d619afd72c13dec1460cd2b9fa8709e9c0c3d0aa
humberto2552 <223110818@upmh.edu.mx>
2025-07-10 16:24:50 -0600

    Corrijo app.py y actualizo cambios

--- Serpientes y escaleras/app.py
+++ Serpientes y escaleras/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import random
+import os
 
 app = Flask(__name__)
 app.secret_key = "clave_secreta"
@@ -146,4 +147,5 @@ def seleccion_estado():
 
 
 if __name__ == "__main__":
-    app.run(host="192.180.4.129", port="5050", debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
